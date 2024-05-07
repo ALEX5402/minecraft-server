73e566a224697624a5231f3d3fc24581e9bb2bda
Alex <alex5402private@gmail.com>
2024-05-07 05:03:06 +0000

    done

--- startpush.sh
+++ startpush.sh
@@ -2,7 +2,7 @@
 
 while true; do
     echo "backup ...."
-    bash autocommit.sh  
+    bash autocommit.sh
     sleep 10800  # Sleep for 3 hours (3 hours * 60 minutes * 60 seconds = 10800 seconds)
 done
 
