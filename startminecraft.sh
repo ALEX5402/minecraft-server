1a2637050b99b02a10e09bb821adff41d8d96804
Alex <alex5402private@gmail.com>
2024-05-07 05:49:04 +0000

    update Tue 07 May 2024 05:49:04 AM UTC

--- startminecraft.sh
+++ startminecraft.sh
@@ -4,10 +4,10 @@ while true; do
     if screen -ls | grep -q "zindahubhai"; then
         echo "Screen session 'zindahubhai' found."
         # Add your actions here if the screen session is found
-        bash startserver.sh
       else
         echo "Screen session 'zindahubhai' not found."
         # Add your actions here if the screen session is not found
+        ./startserver.sh
     fi
     sleep 5
 done
--- startserver.sh
+++ startserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -S zindahubhai -d -m java -jar server.jar
+screen -S zindahubhai -d -m java -Xms128M -XX:MaxRAMPercentage=90 -jar server.jar
