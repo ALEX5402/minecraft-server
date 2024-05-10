4e404e5443c4ff4e5c00f586e5b52e65bfa0b4ef
Alex <alex5402private@gmail.com>
2024-05-10 12:30:15 +0530

    Update startminecraft.sh

--- startminecraft.sh
+++ startminecraft.sh
@@ -3,10 +3,8 @@
 while true; do
     if screen -ls | grep -q "zindahubhai"; then
         echo "Screen session 'zindahubhai' found."
-        # Add your actions here if the screen session is found
       else
         echo "Screen session 'zindahubhai' not found."
-        # Add your actions here if the screen session is not found
         ./startserver.sh
     fi
     sleep 5
